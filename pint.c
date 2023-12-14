7c10f68722a3e8a40d8ff55b881d2d0f8750d96a
Michael Megbenu <msk.megbenu@gmail.com>
2023-12-15 00:28:23 +0200

    POSIX ERROR

--- pint.c
+++ pint.c
@@ -2,14 +2,14 @@
 /**
  * pint_fxn - prints the top
  * @head: stack head
- * @counter: line_number
+ * @number: line_number
  * Return: no return
 */
-void pint_fxn(stack_t **head, unsigned int counter)
+void pint_fxn(stack_t **head, unsigned int number)
 {
 	if (*head == NULL)
 	{
-		fprintf(stderr, "L%u: can't pint, stack empty\n", counter);
+		fprintf(stderr, "L%u: can't pint, stack empty\n", number);
 		fclose(train.file);
 		free(train.streamptr);
 		freestk(*head);
